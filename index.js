0819aba2d5a071315caa1cca090973778ff93686
kewalpasad <kewal2123@gmail.com>
2022-05-17 18:17:55 -0400

    lint fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { ApolloServer } = require("apollo-server");
-const { typeDefs } = require("./schema/type-defs");
-const { resolvers } = require("./schema/resolvers");
+const { ApolloServer } = require('apollo-server');
+const { typeDefs } = require('./schema/type-defs');
+const { resolvers } = require('./schema/resolvers');
 
-const server = new ApolloServer({ typeDefs, resolvers});
+const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({url}) => {
-  console.log(`YOUR API IS NOW RUNNING AT: ${url} :`)
-})
+server.listen().then(({ url }) => {
+	console.log(`YOUR API IS NOW RUNNING AT: ${url} :`);
+});
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -1,46 +1,46 @@
-const { gql } = require("apollo-server");
+const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-  type User {
-    id: ID!,
-    name: String!,
-    username: String!,
-    age: Int!,
-    nationality: String,
-    friends: [User]
-  }
+	type User {
+		id: ID!
+		name: String!
+		username: String!
+		age: Int!
+		nationality: String
+		friends: [User]
+	}
 
-  type Movie {
-    id: ID!,
-    name: String!,
-    yearOfPublication: Int!,
-    isInTheaters: Boolean!,
-  }
+	type Movie {
+		id: ID!
+		name: String!
+		yearOfPublication: Int!
+		isInTheaters: Boolean!
+	}
 
-  type Query {
-    users: [User!]!,
-    user(id: ID!): User!,
-    Movies: [Movie!]!,
-    Movie(name: String!): Movie!,
-  }
+	type Query {
+		users: [User!]!
+		user(id: ID!): User!
+		Movies: [Movie!]!
+		Movie(name: String!): Movie!
+	}
 
-  input CreateUserInput {
-    name: String!,
-    username: String!,
-    age: Int!,
-    nationality: String
-  }
+	input CreateUserInput {
+		name: String!
+		username: String!
+		age: Int!
+		nationality: String
+	}
 
-  input UpdateUserName {
-    id: ID!,
-    newUserName: String!
-  }
+	input UpdateUserName {
+		id: ID!
+		newUserName: String!
+	}
 
-  type Mutation {
-    createUser(input: CreateUserInput!): User!,
-    updateUserName(input: UpdateUserName) : User!,
-    deleteUser(id: ID!): User
-  }
-`
+	type Mutation {
+		createUser(input: CreateUserInput!): User!
+		updateUserName(input: UpdateUserName): User!
+		deleteUser(id: ID!): User
+	}
+`;
 
-module.exports = {typeDefs}
+module.exports = { typeDefs };
